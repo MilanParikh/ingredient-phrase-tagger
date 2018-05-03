963b6bd124883dd10ec8c3a3d96a15dc03cbb8a8
Michael Lynch <mtlynch@users.noreply.github.com>
2018-05-03 12:10:35 -0400

    Adding docstring for split_labels (#40)

--- ingredient_phrase_tagger/training/partitioner.py
+++ ingredient_phrase_tagger/training/partitioner.py
@@ -2,7 +2,27 @@ def split_labels(label_reader,
                  training_label_writer,
                  testing_label_writer,
                  training_fraction,
-                 max_labels=None):
+                 max_labels=0):
+    """Splits a full label set into a training and testing set.
+
+    Given a full set of labels and associated inputs, splits up the labels
+    into training and testing sets in the proportion defined by
+    training_fraction.
+
+    Args:
+        label_reader: A labelled_data.Reader instance that reads the full set
+            of labels.
+        training_label_writer: A labelled_data.Writer instance that writes out
+            the subset of labels to be used for training.
+        testing_label_writer: A labelled_data.Writer instance that writes out
+            the subset of labels to be used for testing.
+        training_fraction: A value between 0.0 and 1.0 that specifies the
+            proportion of labels to use for training. Any label not used for
+            training is used for testing until max_labels is reached.
+        max_labels: The maximum number of labels to read from label_reader. 0
+            is treated as infinite.
+
+    """
     labels = _read_labels(label_reader, max_labels)
     _write_labels(labels, training_label_writer, testing_label_writer,
                   training_fraction)
